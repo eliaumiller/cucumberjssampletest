60106a3f89444494159eb0c524941a9e6ea6f2c7
zousu <summer.zou@microfocus.com>
2021-08-18 14:08:56 +0800

    test passed bdd, provide passed step def

---
+++ features/step_definitions/passed_steps.js
@@ -0,0 +1,101 @@
+const assert = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+Given('a global administrator named {string}', function (string) {
+       // Write code here that turns the phrase above into concrete actions
+    this.name = string;
+});
+
+
+Given('a blog named {string}', function (string) {
+       // Write code here that turns the phrase above into concrete actions
+    this.blog = string;
+});
+
+
+Given('a customer named {string}', function (string) {
+       // Write code here that turns the phrase above into concrete actions
+    this.customer = string;
+});
+
+
+Given('the following people exist:', function () {
+       // Write code here that turns the phrase above into concrete actions
+    this.people = {};
+});
+
+
+Given('some precondition {int}', function (int) {
+     // Given('some precondition {float}', function (float) {
+       // Write code here that turns the phrase above into concrete actions
+    this.precondition = int;
+});
+
+
+When('some action by the actor', function () {
+       // Write code here that turns the phrase above into concrete actions
+    this.action = '';
+});
+
+
+When('some other action', function () {
+       // Write code here that turns the phrase above into concrete actions
+    this.otherAction = '';
+});
+
+
+
+Then('some testable outcome is achieved', function () {
+       // Write code here that turns the phrase above into concrete actions
+    this.outcome = true;
+});
+
+
+Then('something else we can check happens too', function () {
+       // Write code here that turns the phrase above into concrete actions
+    this.otherHappens = true;
+});
+
+
+Given('some precondition', function () {
+       // Write code here that turns the phrase above into concrete actions
+    this.pre = 1;
+});
+
+
+Given('some other precondition with doc string', function () {
+       // Write code here that turns the phrase above into concrete actions
+    this.otherPrecondition = '';
+});
+
+
+When('yet another action', function () {
+       // Write code here that turns the phrase above into concrete actions
+    this.anotherAction = '';
+});
+
+
+Given('I don\'t see something else', function () {
+       // Write code here that turns the phrase above into concrete actions
+    this.result = false;
+});
+
+
+Given('the cow weighs {int} kg', function (int) {
+     // Given('the cow weighs {float} kg', function (float) {
+       // Write code here that turns the phrase above into concrete actions
+    this.weights = int;
+});
+
+
+When('we calculate the feeding requirements', function () {
+       // Write code here that turns the phrase above into concrete actions
+    this.calculate = true;
+});
+
+
+Then('the energy should be {int} MJ', function (int) {
+     // Then('the energy should be {float} MJ', function (float) {
+       // Write code here that turns the phrase above into concrete actions
+    this.energy = int;
+});
